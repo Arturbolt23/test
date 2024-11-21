7054d77d1e6d192d9ce26e3b404d413238423270
Artyr <boltinovartua@gmail.com>
2024-11-21 15:11:55 +0300

    тестовое добавление в документацию

---
+++ fake_math.py
@@ -0,0 +1,14 @@
+
+def fake_math(first, second):
+    if second != 0 :
+        result = first/ second
+        return result
+    else :
+        return 'Ошибка'
+
+
+# fake_math(49, 7)
+# fake_math(7, 0)
+
+
+
---
+++ true_math.py
@@ -0,0 +1,11 @@
+from math import inf
+
+def true_math(first, second):
+    if second != 0 :
+        result = first/ second
+        return result
+    else :
+        return inf
+
+# true_math(47, 5)
+# true_math(48, 0)
